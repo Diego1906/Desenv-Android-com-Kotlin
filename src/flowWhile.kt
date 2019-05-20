934414ea6d3b18da6d73891aaa0eed6cd161f8a4
Diego Santos <Diego.Santos@target.local>
2019-05-20 20:31:35 -0300

    Alterações da Aula 20.05.19

---
+++ src/flowWhile.kt
@@ -0,0 +1,8 @@
+fun main() {
+    var l: Int = 10
+    do {
+        println(l)
+        l += 5
+    } while (l <= 100)
+}
+
---
+++ src/main.kt
@@ -0,0 +1,4 @@
+fun endereco(rua: String = "", cidade: String, estado: String, cep: String, numero: Int = 0) {
+    println("Rua:  $rua")
+    println("Cidade:  $cidade,  $estado - $cep")
+}
